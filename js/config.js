cf903e7137a60a78906f974c8080f32cb62219a9
Andrey Dokuchayev <andreydoku@gmail.com>
2023-12-20 17:13:47 -0600

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_BKkbOwZt8',
+        userPoolClientId: '3i58qavffhd0jg5ern9qd81nho',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
